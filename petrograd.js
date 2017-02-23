5f3ca44e20e428fe31da70863d74812cfe5ed017
André Vestergaard <Andr Vestergaard>
2017-02-23 11:01:52 +0100

    load JSON produktliste and create template-clone for each

--- petrograd.js
+++ petrograd.js
@@ -5,13 +5,20 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("Siden vises");
+
+    //læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
     visProdukt();
-
-    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visPersonListe);
-
 }
 
-function visProdukt() {
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt(produkt) {
+    console.log(produkt);
     // klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 
